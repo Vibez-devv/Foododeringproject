7457def26792bbf597ce654af6f204e3decc048c
vibez-devv <abdulrasheedabdulkadir554@gmail.com>
2026-09-19 21:09:07 +0100

    Fix Login CSS filename

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import "./login.css";
+import "./Login.css";
 
 function Login() {
   const navigate = useNavigate();
